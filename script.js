312d7e9bcb6cc38b8438a8a910eedecaa99dd30a
Calder Dodd Russell <26crussell@cpsd.us>
2024-10-27 09:56:30 -0400

    Added the first cod for Css Js and H

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.addEventListener('scroll', function() {
+    const quotes = document.querySelectorAll('.quote');
+    const scrollPosition = window.scrollY;
 
+    quotes.forEach((quote, index) => {
+        if (scrollPosition > index * 400) {
+            quote.style.opacity = 1;
+            quote.style.transform = 'translateY(0)';
+        }
+    });
+});
